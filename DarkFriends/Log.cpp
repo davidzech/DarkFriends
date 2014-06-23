5663ab14dca25d40dff01a776515d6b0126fdfa1
David Zech <david.zech@gmail.com>
2014-06-23 14:30:37 -0700

    Log to file and get rid of MFC

--- DarkFriends/Log.cpp
+++ DarkFriends/Log.cpp
@@ -6,12 +6,13 @@
 using std::string;
 
 extern HMODULE thisModule;
+extern FILE* logOut;
 
 void Log(std::string format, ...) 
 {
 	va_list args;
 	va_start(args, format);
-	vprintf(format.c_str(), args); //this needs to write to file next to dll
+	vfprintf(logOut, format.c_str(), args); //this needs to write to file next to dll
 	va_end(args);
 
 }
--- DarkFriends/dllmain.cpp
+++ DarkFriends/dllmain.cpp
@@ -2,9 +2,11 @@
 #include "stdafx.h"
 #include "DarkFriends.h"
 #include "Log.h"
+#include <string>
 
 HMODULE steamHandle;
 HMODULE thisModule;
+FILE *logOut = nullptr;
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -14,15 +16,23 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-	{
-		Log("Attached");
-		DisableThreadLibraryCalls(hModule);
-		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&Bootstrap, hModule, 0, NULL);
-		break;
-	}
-	case DLL_PROCESS_DETACH:
-		Destroy();
-		break;
+		{
+			char fileName[MAX_PATH];
+			GetModuleFileName(hModule, fileName, MAX_PATH);
+			std::string dll = fileName;
+			dll += ".log";
+			if (fopen_s(&logOut, dll.c_str(), "a+") != 0)
+			{
+				logOut = stdout;
+			}
+			Log("Attached");
+			DisableThreadLibraryCalls(hModule);
+			CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&Bootstrap, hModule, 0, NULL);
+			break;
+		}
+		case DLL_PROCESS_DETACH:
+			Destroy();
+			break;
 	}
 	return TRUE;
 }
